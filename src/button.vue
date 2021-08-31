7e09f49f17fb4d28383403a957cb345da8662ae8
andy.xing <991376602@qq.com>
2021-08-31 22:39:43 +0800

    添加了icon，并验证iconPosition是right或left

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,27 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-export default {}
+export default {
+  //props:['icon','iconPosition']
+  props:{
+    icon:{},
+    iconPosition:{
+      type:String,
+      default:'left',
+      validator(value){
+        return !(value !== 'left' && value !== 'right');
+      }//验证iconPosition属性
+    }
+  }
+}
 </script>
 <style lang="scss">
 .g-button {
@@ -12,6 +31,10 @@ export default {}
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;//inline-flex会引起不对齐，直接用这个可以解决
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -20,6 +43,20 @@ export default {}
   }
   &:focus {
     outline: none;
+  }
+  >.icon{
+    order: 1;margin-right: .1em;
+  }
+  >.content{
+    order: 2;
+  }
+  &.icon-right{
+    .icon{
+      order: 2;margin-left: .1em;
+    }
+    .content{
+      order: 1;
+    }
   }
 }
 
